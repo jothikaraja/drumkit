90710ed8ca41f04e0072688f214d9facc8bb80e8
JOTHIKA <jothika.zero@gmail.com>
2020-07-09 15:53:25 +0530

    final

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 
- 
-
-$("h1").text("hello");
 var btns=document.querySelectorAll("button");
 var aud=["sounds/tom-1.mp3","sounds/tom-2.mp3","sounds/tom-3.mp3","sounds/tom-4.mp3","sounds/crash.mp3","sounds/kick-bass.mp3","sounds/snare.mp3"];
 
@@ -9,8 +6,7 @@ window.addEventListener("keydown",press);
 
 for(var i=0;i<7;i++)
 {
-	/*btns[i].addEventListener("click",handleclick);
-	btns[i].addEventListener("click",changebg);*/
+	
 
     btns[i].addEventListener("click",playsound);
 
@@ -40,13 +36,3 @@ function playsound(){
 	var audio = new Audio(aud[x]);
 	audio.play();
 }
-
-
-/*function handleclick(){
-	alert("i got clicked");
-}
-function changebg(){
-	this.style.backgroundColor="red";
-
-
-}*/
